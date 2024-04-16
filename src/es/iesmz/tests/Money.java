59ca056b8fc32fb182c27c8b77079d200a3208e5
dam_evh <hiloriospoy@gmail.com>
2024-04-16 13:29:21 +0200

    2-> Creo que entiendo que lo del enum y lo de las constantes simplemente era que son 2 cosas diferentes

--- src/es/iesmz/tests/Money.java
+++ src/es/iesmz/tests/Money.java
@@ -1,7 +1,12 @@
 package es.iesmz.tests;
 
-public class Money {
+import java.util.Objects;
 
+public class Money {
+private static final float eur_usd = 1.18798f;
+private static final float usd_eur = 0.841815f;
+private static final float eur_gbp = 0.857839f;
+private static final float gbp_eur = 1.165826f;
     /*
         No conseguí hacer del enum variables constantes de tipo final dentro de la clase
         private final TipoMoneda eur = TipoMoneda.EUR;
@@ -9,12 +14,23 @@ public class Money {
         : (
      */
 
-
+// no se hacer esto, el change
     public float change(TipoMoneda origen, TipoMoneda destino, float money){
-        if (origen != TipoMoneda.EUR && origen != TipoMoneda.USD && origen != TipoMoneda.GBP ||
+        float resultado;
+        if (origen != TipoMoneda.EUR && origen != TipoMoneda.USD && origen != TipoMoneda.GBP
+                ||
                 destino != TipoMoneda.EUR && destino != TipoMoneda.USD && destino != TipoMoneda.GBP){
 
-            
+
+                                    //DE EUR A USD
+            if (Objects.equals(origen, TipoMoneda.EUR) && destino.equals(TipoMoneda.USD)){
+                resultado = money*eur_usd;
+                return resultado;
+            }
+                                    //DE USD A EUR
+            else if (origen.equals(TipoMoneda.USD) && destino.equals(TipoMoneda.EUR)){
+                resultado = money*usd_eur;
+            }
 
 
         }
---
+++ src/es/iesmz/tests/MoneyTest.java
@@ -0,0 +1,12 @@
+package es.iesmz.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+
+    @Test
+
+    m.change
+
+
+}
